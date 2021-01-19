561f329f3f334b14ab3172d67634ee9ab9400e80
Никита <70635031+Nikita7181@users.noreply.github.com>
2021-01-19 22:35:05 +0300

    Update main.cpp

--- lb4/main.cpp
+++ lb4/main.cpp
@@ -6,27 +6,37 @@ using std::endl;
 using std::cout;
 using std::cin;
 
-void swap(std::vector<int>& v, int x, int y);
+void swap(std::vector<int>& v, int x, int y)
+{
+    int temp = v[x];
+    v[x] = v[y];
+    v[y] = temp;
+    
+}
 
-void quicksort(std::vector<int> &vec, int L, int R) {
+void quicksort(std::vector<int> &vec, int L, int R)
+{
     int i, j, mid, piv;
     i = L;
     j = R;
     mid = L + (R - L) / 2;
     piv = vec[mid];
-
-    while (i<R || j>L) {
+    
+    while (i<R || j>L)
+    {
         while (vec[i] < piv)
             i++;
         while (vec[j] > piv)
             j--;
-
-        if (i <= j) {
+        
+        if (i <= j)
+        {
             swap(vec, i, j);
             i++;
             j--;
         }
-        else {
+        else
+            {
             if (i < R)
                 quicksort(vec, i, R);
             if (j > L)
@@ -36,21 +46,13 @@ void quicksort(std::vector<int> &vec, int L, int R) {
     }
 }
 
-void swap(std::vector<int>& v, int x, int y) {
-    int temp = v[x];
-    v[x] = v[y];
-    v[y] = temp;
-
-}
-
-std::vector<int> SortMass_V2(std::vector<int> vector)
+std::vector<int> insert_sort(std::vector<int> vector)
 {
-    int j=0;
-    for( int i=1; i<vector.size();++i){
-        for (int i=j; j>0 && vector[j-1]>vector[j];--j){
-
-            std::swap(vector[j-i],vector[j]);
-
+    for( int i=1; i<vector.size();++i)
+    {
+        for (int j=i; j>0 && vector[j-1]>vector[j];--j)
+        {
+            std::swap(vector[j - i], vector[j]);
         }
     }
     return vector;
@@ -60,7 +62,8 @@ std::vector<int> Fillarray(int size)
 {
     std::vector<int> arr;
     std::srand(std::time(nullptr));
-    for (int i = 0; i <= size; i++) {
+    for (int i = 0; i <= size; i++)
+    {
         arr.push_back(std::rand() % 201 - 100);
     }
     return arr;
@@ -69,10 +72,13 @@ std::vector<int> Fillarray(int size)
 std::vector<int> SortMass(std::vector<int> vector)
 {
     bool h = true;
-    while (h) {
+    while (h)
+    {
         h = false;
-        for (int i = 0; i < vector.size() - 1; ++i) {
-            if (vector[i] > vector[i + 1]) {
+        for (int i = 0; i < vector.size() - 1; ++i)
+        {
+            if (vector[i] > vector[i + 1])
+            {
                 h = true;
                 std::swap(vector[i], vector[i + 1]);
             }
@@ -83,46 +89,47 @@ std::vector<int> SortMass(std::vector<int> vector)
 
 int main ()
 {
+    int size;
     cout << "Task 1" << endl;
-    int n;
-    cout << "n= " << endl;
-    cin >> n;
-    std::vector<int>b=Fillarray(n);
-    for(int i : b){
+    cout << "size= " ;
+    cin >> size;
+    std::vector<int>b=Fillarray(size);
+    for(int i : b)
+    {
         cout << i << endl;
     }
     cout << "filter" << endl;
     std::vector<int >output = SortMass(b);
-    for(int i : output){
+    for(int i : output)
+    {
         cout << i << endl;
     }
     cout << "Task 2" << endl;
-    int m;
-    cout << "m= " << endl;
-    cin >> m;
-    std::vector<int>a=Fillarray(m);
-    for(int i : a){
+    cout << "size= ";
+    cin >> size;
+    std::vector<int> a = Fillarray(size);
+    for(int i : a)
+    {
         cout << i << endl;
     }
     cout << "filter" << endl;
-    std::vector<int>output2=SortMass_V2(a);
-    for(int i : output2){
+    std::vector<int> output2 = insert_sort(a);
+    for(int i : output2)
+    {
         cout << i << endl;
     }
     cout << "Task 3" << endl;
-    std::vector<int> vec1;
-    cout << "k= " << endl;
-    int k;
-    cin >> k;
-    for (int i = 0; i < k; i++) {
-        vec1.push_back(1 + rand() % 100);
-    }
-    for(int i : vec1){
+    cout << "size= ";
+    cin >> size;
+    std::vector<int> vec1 = Fillarray(size);
+    for(int i : vec1)
+    {
         cout << i << endl;
     }
     cout << "filter" << endl;
-    quicksort(vec1, 0, k - 1);
-    for(int i : vec1){
+    quicksort(vec1, 0, size - 1);
+    for(int i : vec1)
+    {
         cout << i << endl;
     }
     return 0;
